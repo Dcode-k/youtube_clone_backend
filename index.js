70dbcc39216369c2ff3b070784e3eb7e70c9f718
Dcode-k <Krishnkantpachori50@gmail.com>
2023-09-18 16:07:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch';
+import express from 'express';
+import cors from 'cors';
+
+
+const PORT=5000;
+const app=express();
+
+app.use(cors());
+const corsOptions={
+    origin:"http://localhost:3000"
+}
+
+const requestEndpoint="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+app.get('/getData/:data', cors(corsOptions), async (req, res) => {
+    const fetchOptions = {
+        method: 'GET'
+    }
+    const response = await fetch( `${requestEndpoint}${req.params.data}`, fetchOptions);
+    const jsonResponse = await response.json();
+    res.json(jsonResponse);
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
